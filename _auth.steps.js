cc081e4a503c565e0fdcc6355737922f7accea54
MORSHED-HOME <morshed952640@gmail.com>
2022-10-17 22:32:25 +0600

    Some Practices added

--- _auth.steps.js
+++ _auth.steps.js
@@ -1,5 +1,7 @@
 /* 
+    --------------
     AUTH SETUP 
+    --------------
     1. create firebase project
     2. enable web 
     3. enable sign in method
@@ -8,10 +10,13 @@
     6. export app from firebase.config.js
 
 */
-    
+
 /* 
+    ----------------
+    CONTEXT API SETUP
+    ----------------
     1. Create UserContext (AuthContext): UserContext --> component name UserContext provides AuthContext
-    2. Create authContext
+    2. Create AuthContext
     3. set AuthContext.Provider
     4. Make sure you set the children
     5. export AuthContext to be used inside useContext hook
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -16,6 +16,9 @@ export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const UserContext = ({ children }) => {
+
+  console.log(children);
+
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -44,6 +47,7 @@ const UserContext = ({ children }) => {
   }, []);
 
   const authInfo = { user, loading, createUser, signIn, logOut };
+  
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
